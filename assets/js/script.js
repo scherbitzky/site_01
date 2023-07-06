49f42a36cca2d904a62a356fbf54a739c62a9a0d
shcherbitzky <136928749+shcherbitzky@users.noreply.github.com>
2023-07-06 22:23:33 +0300

    Set of changes
    Set of changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,43 @@
 'use strict';
-// mobile menu
+// Show or hide mobile menu
 const menuIcon = document.querySelector('.menu__icon');
-if (menuIcon) {
-    const menuBody = document.querySelector('.menu__body');
-    if (menuBody) {
-        menuIcon.addEventListener('click', () => {
+const menuBody = document.querySelector('.menu__body');
+if (menuIcon && menuBody) {
+    menuIcon.addEventListener('click', () => {
+        document.body.classList.toggle('_lock');
+        menuIcon.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+// Smooth translate to subscribe section
+const subscribeLinks = document.querySelectorAll('a[data-goto]');
+const menuLinks = document.querySelectorAll('.menu__link');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', () => {
             document.body.classList.toggle('_lock');
             menuIcon.classList.toggle('_active');
             menuBody.classList.toggle('_active');
         });
-    }
+    });
+}
+if (subscribeLinks.length > 0) {
+    subscribeLinks.forEach(subscribeLink => {
+        subscribeLink.addEventListener('click', (e) =>{
+            const data = e.target.dataset.goto;
+            if (data && document.querySelector(data)) {
+                const gotoBlock = document.querySelector(data);
+                let gotoBlockPosition = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('.header').offsetHeight;
+                smoothScroll({yPos: gotoBlockPosition, duration: 1000});
+                document.querySelector('.form-subscribe__input').focus();
+                e.preventDefault();
+            }
+        });
+    })
 }
 
-// smooth appearance of sections
+// Smooth appearance of sections
 function onEntry(entry) {
     entry.forEach(change => {
         if (change.isIntersecting) {
@@ -22,14 +47,14 @@ function onEntry(entry) {
         }
     });
 }
-let options = { threshold: [0.1] };
+let options = { threshold: [0.01] };
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('section');
 for (let elm of elements) {
     observer.observe(elm);
 }
 
-// feedback swicher
+// Feedback swicher
 const btnNext = document.querySelector('.flipping__next');
 const btnBack = document.querySelector('.flipping__back');
 const feedbackItems = document.querySelectorAll('.testimonials__feedback');
@@ -71,4 +96,13 @@ if (btnNext && btnBack) {
             toggleClass();
         }
     });
+}
+
+// Interval "background-image" change
+const historyContainer = document.querySelector('.history__container');
+if (historyContainer) {
+    function handler() {
+        historyContainer.classList.toggle('_bg-alternate');
+    }
+    setInterval(handler, 5000);
 }
